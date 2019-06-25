7478b246a8a49d1b61d13800e0d7285984ac0b6e
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-06-25 15:13:26 -0700

    Adds some styling and structure to the product detail page

--- client/src/components/products/ProductIndex.js
+++ client/src/components/products/ProductIndex.js
@@ -21,12 +21,12 @@ const ProductIndex = () => {
                     <div className="product-list-item-top"></div>
                     <div className="product-list-item-image" style={{backgroundImage: `url(${product.image_url})`}}/>
                     <div className="product-list-item-body">
-                      <h3>{product.name}</h3>
-                      <span>${product.price}</span>
+                      <h3 className="product-list-item-name">{product.name}</h3>
+                      <span className="product-list-item-price">${product.price}</span>
                       <div className="product-list-item-bottom">
                         <div className="product-list-item-row">
-                          <span>&#9733;&#9733;&#9733;&#9733;&#9733;</span>
-                          <span>{product.genre.name.toUpperCase()}</span>
+                          <span className="product-list-item-rating">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+                          <span className="product-list-item-genre">{product.genre.name.toUpperCase()}</span>
                         </div>
                       </div>
                     </div>
