b3b3bd34b4936390b80069be096663a637482748
david <dsuon04@gmail.com>
2019-06-26 22:35:55 -0700

    splash i guess

--- client/src/components/main/main_page.js
+++ client/src/components/main/main_page.js
@@ -3,16 +3,28 @@ import React, { Component } from "react";
 import GenreIndex from "../genres/GenreIndex"
 import Carousel from '../carousel/carousel'
 import '../../styles/main.css'
+// import Must from './mustache.png' ;
 export class Main extends Component {
   render() {
     return (
       <div className="splash-main-container">
         <h1 style={{textAlign: "center", margin: "8px"}}>Handlebar: An Online Store (navbar me)</h1>
         <div className="caro-div-container">
-          <div>
-            <p>Playstation</p>
-          </div>
           <Carousel />
+          <div className="splash-description-container" >
+            <div className="splash-description">
+              <h1 className="splash-description-header"> What is Handlebar?</h1>
+              <p className="splash-description-text">
+                Handlebar is a nifty website that allows user to market their goods, 
+                as well as discover new goods related to their interest.  
+                There's plenty of genres to choose 
+                and more being added from other 
+                user so you're certain to find something you like.
+                Not just a stache, it's goods.
+                
+              </p>
+            </div>
+          </div>
         </div>
 
         <GenreIndex />
