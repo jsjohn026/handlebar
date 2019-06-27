14a7a2d24b12fdecebc0c56daadc31b33d1559c4
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-06-27 15:15:17 -0700

    Adds styling to the create product fomr

--- client/src/components/products/ProductCreate.js
+++ client/src/components/products/ProductCreate.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withApollo, Mutation } from "react-apollo";
 import { FETCH_GENRES } from "../../graphql/queries";
 import { CREATE_PRODUCT } from "../../graphql/mutations";
+import "../../styles/product_create.css";
 
 class ProductCreate extends React.Component {
   constructor(props) {
@@ -51,7 +52,9 @@ class ProductCreate extends React.Component {
     let genreOptions;
     if (this.state.genres) {
       genreOptions = this.state.genres.map(genre => (
-        <option key={genre._id} value={genre._id}>{genre.name.slice(0,1).toUpperCase() + genre.name.slice(1)}</option>
+        <option key={genre._id} className="create-product-option" value={genre._id}>
+          {genre.name.slice(0,1).toUpperCase() + genre.name.slice(1)}
+        </option>
       ));
     }
 
@@ -59,18 +62,50 @@ class ProductCreate extends React.Component {
       <Mutation mutation={CREATE_PRODUCT}>
         {createProduct => {
           return (
-            <div>
-              <form onSubmit={event => this.handleSubmit(event, createProduct)}>
-                <input value={this.state.name} onChange={this.updateInput("name")}></input>
-                <input value={this.state.description} onChange={this.updateInput("description")}></input>
-                <input value={this.state.image_url} onChange={this.updateInput("image_url")}></input>
-                <input value={this.state.price} onChange={this.updateInput("price")}></input>
-                <select defaultValue="" onChange={this.updateInput("genre")}>
-                  <option value="">Choose a genre...</option>
-                  { genreOptions }
-                </select>
-                <button>Submit</button>
-              </form>
+            <div className="create-product-page">
+              <div className="create-product-form-container">
+                <h1 className="create-product-header">Add Listing</h1>
+                <form className="create-product-form" onSubmit={event => this.handleSubmit(event, createProduct)}>
+                  <div className="create-product-title-container">
+                    <h2 className="create-product-form-header">Title</h2>
+                    <p className="create-product-main-info">Use words that people would search for when looking for your item</p>
+                    <input className="create-product-input" value={this.state.name} onChange={this.updateInput("name")}/>
+                    <p className="create-product-light-info">Need some inspiration? Include details such as brand, color, size, specs, condition, etc.</p>
+                  </div>
+                  <div className="create-product-image_url-container">
+                    <h2 className="create-product-form-header">Add a photo</h2>
+                    <p className="create-product-main-info">Get people excited in your item! (Make sure it's a good photo!)</p>
+                    <input className="create-product-input" value={this.state.image_url} onChange={this.updateInput("image_url")}/>
+                    <p className="create-product-light-info">No pressure...</p>
+                  </div>
+                  <div className="create-product-description-genre-container">
+                    <div className="create-product-description-container">
+                      <h2 className="create-product-form-header">Description</h2>
+                      <p className="create-product-main-info">Tell us more about your item</p>
+                      <textarea className="create-product-textarea" value={this.state.description} onChange={this.updateInput("description")}/>
+                    </div>
+                    <div className="create-product-genre-container">
+                      <h2 className="create-product-form-header">Pick a genre</h2>
+                      <p className="create-product-main-info">How would you classify your item?</p>
+                      <select className="create-product-select" defaultValue="" onChange={this.updateInput("genre")}>
+                        <option className="create-product-option" value="">Choose a genre...</option>
+                        { genreOptions }
+                      </select>
+                      <p className="create-product-light-info">If what you had in mind is not there, we are sorry</p>
+                    </div>
+                  </div>
+                  <div className="create-product-price-container">
+                    <div>
+                      <h2 className="create-product-form-header">Price</h2>
+                      <input className="create-product-input" value={this.state.price} onChange={this.updateInput("price")}></input>
+                      <p className="create-product-light-info">So, how much do you want for this thing?</p>
+                    </div>
+                    <div className="create-product-submit-container">
+                      <button className="create-product-submit">List Your Item!</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           );
         }}
