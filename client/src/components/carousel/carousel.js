ccfeb676e8951902c3b3adcc10a492abe4040c0c
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-07-15 23:15:47 -0700

    Adds more pictures to the carousel

--- client/src/components/carousel/carousel.js
+++ client/src/components/carousel/carousel.js
@@ -16,11 +16,11 @@ class DemoCarousel extends Component {
                 </div>
                 
                 <div>
-                    <img src="https://bciconline.com/wp-content/uploads/2017/10/2.png" alt="not found" />
+                    <img src="https://cdn2.shopify.com/s/files/1/0019/9511/2563/products/Gem_Water_Beauty_Water_Botlle_1200x.JPG?v=1553797633" alt="not found" />
                     
                 </div>
                 <div>
-                    <img src="https://i.redd.it/0cscovjonpi11.png" alt="not found" />
+                    <img src="https://i.pinimg.com/originals/52/31/df/5231df95dadf9ef080a2de1777bfa0f6.jpg" alt="not found" />
                     
                 </div>
             </Carousel>
