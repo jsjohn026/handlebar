12537bef6ff2f2bd434538be3e255183af621a21
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-07-16 10:54:11 -0700

    Fixes variable declaration

--- server/server.js
+++ server/server.js
@@ -9,16 +9,16 @@ const schema = require("./schema/schema");
 const cors = require("cors");
 const path = require("path");
 
+const app = express();
+if (!db) {
+  throw new Error("You must provide a string to connect to MongoDB Atlas");
+}
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 	app.get('/', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
-}
-
-const app = express();
-if (!db) {
-  throw new Error("You must provide a string to connect to MongoDB Atlas");
 }
 
 mongoose
